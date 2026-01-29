de2b1b1dd11870e93d4e5e0415d7d59773f369c2
Chad Redmond <credmond@mercyhurst.edu>
2026-01-29 01:02:54 +0000

    first commit

---
+++ jan28/fetch.py
@@ -0,0 +1,19 @@
+import sqlite3
+import pandas as pd
+
+
+conn = sqlite3.connect('baseball.db')
+cursor = conn.cursor()
+query = """
+   SELECT teamID,sum(HR) as seasonHR
+   FROM batting
+   WHERE yearID = 2025
+   GROUP BY teamID
+   ORDER BY seasonHR desc
+"""
+cursor.execute(query)
+records = cursor.fetchall()
+conn.close()
+
+records_df = pd.DataFrame(records,columns = ["teamID","seasonHR"])
+print(records_df)
